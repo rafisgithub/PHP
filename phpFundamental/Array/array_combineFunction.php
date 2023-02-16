90d4aeeb285f64983ff1f828dc66c5cb77581355
Rafi <smrafi120@gmail.com>
2023-02-16 10:19:32 +0600

    array combine

--- phpFundamental/Array/array_combineFunction.php
+++ phpFundamental/Array/array_combineFunction.php
@@ -1,14 +1,25 @@
 <?php include "./header.php" ?>
 
-<h1>Php Array Function - array_column()</h1>
+<h1>Php Array Function - array_combine()</h1>
 
 <hr>
 <section class="content">
 
 <?php
 
+$name = array(" Rafi ","Shakib","Meghla",);
+$department = array("CSE","Math","AI",);
+
+$combine = array_combine($name, $department);
 
 
+print("<pre>");
+var_dump($combine) ."<br>";
+var_export($combine) ."<br>";
+print_r($combine)."<br>";
+
+
+print("<pre>");
  ?>
 
 </section>
