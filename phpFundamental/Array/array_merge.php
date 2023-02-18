b2ba979bd1e2ee7d1b4139989a9e3b9943bd0dae
Rafi <smrafi120@gmail.com>
2023-02-18 21:05:33 +0600

    arry merge

--- phpFundamental/Array/array_merge.php
+++ phpFundamental/Array/array_merge.php
@@ -7,8 +7,33 @@
 
 
     <?php
+    $arr1 = array("red", "green");
+    $arr2 = array("blue", "white");
 
-    
+    $reuslt = array_merge($arr1, $arr2);
+
+    print("<pre>");
+    print_r($reuslt);
+
+    echo "<hr>";
+
+
+    $arr1 = array (
+        "a" => "red",
+        "b" => "green",
+    );
+
+    $arr2 = array(
+        "c" => "blue",
+        "b" => "white",
+    );
+
+    $reuslt = $arr1+ $arr2;
+    print("<pre>");
+    print_r($reuslt);
+
+
+
      ?>
 </section>
 <?php include('./footer.php'); ?>
