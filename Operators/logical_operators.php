92e4560b1149598a4e8cdfc5f83ab2ea1778fc36
rafisgithub <rafi.cse.ahmed@gmail.com>
2026-05-15 12:49:20 +0600

    Add logical operators demonstration script

---
+++ Operators/logical_operators.php
@@ -0,0 +1,11 @@
+<?php
+
+$age = 25;
+
+if($age >= 18 && $age <= 30) {
+    echo "You are eligible for the youth program.\n";
+} else {
+    echo "You are not eligible for the youth program.\n";
+}
+
+?>
