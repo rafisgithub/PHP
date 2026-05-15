50b8533c959c556c744ee346bb8d63f3ad1aea9f
rafisgithub <rafi.cse.ahmed@gmail.com>
2026-05-15 12:31:08 +0600

    Initial commit

---
+++ Operators/arithmetic_operators.php
@@ -0,0 +1,25 @@
+<?php
+// Arithmatic Operators
+$x = 10;
+$y = 15;
+
+// Addition
+$sum = $x + $y;
+echo "Sum is = " . $sum . "\n";
+
+// Subtraction
+$subtraction = $x - $y;
+echo "Subtraction is = " . $subtraction . "\n";
+
+// Multiplication
+$mul = $x * $y;
+echo "Multiplication is = " . $mul . "\n";
+
+// Division
+$div = $x / $y;
+echo "Division is = " . $div . "\n";
+
+
+
+
+?>
